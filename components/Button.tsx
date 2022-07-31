40c7482cf7cc3081091895a1a8050556553ff7e6
jenesh <jenesh@dzangolab.com>
2022-07-31 15:00:37 +0545

    Feat(UI) - update button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,28 @@
 import type { ButtonHTMLAttributes, ReactNode } from "react";
+import Spinner from "./Spinner";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: ReactNode;
-  fullWidth?: Boolean;
+  fullWidth?: boolean;
+  isSpinning?: boolean;
 };
 
-const Button = ({ children, fullWidth = false }: ButtonProps) => {
+const Button = ({
+  children,
+  fullWidth = false,
+  isSpinning = false,
+  ...buttonProps
+}: ButtonProps) => {
   return (
     <button
-      className={`bg-blue-500/20 hover:bg-blue-500/30 text-blue-500 px-4 py-2 ${
+      className={`flex gap-2 items-center justify-center bg-blue-500/20 hover:bg-blue-500/30 text-blue-500 px-4 py-2 ${
         fullWidth && "w-full"
-      }`}
+      }
+      disabled:cursor-not-allowed disabled:bg-blue-300/10 disabled:hover:bg-blue-300/10 disabled:text-blue-500/50
+      `}
+      {...buttonProps}
     >
+      {isSpinning && <Spinner />}
       {children}
     </button>
   );
